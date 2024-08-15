f1ae24e2f33193fba187089d0516cb5de528a2ea
Roman Geber <dlz9zxdkjptu@romangeber.com>
2024-08-15 09:47:29 +0200

    Integrated current surreadb.js beta (beta 9 is fudged). Using npm readme example with modified imports, etc. as a proof of concept. Note that putting the example into the js block without `onBeforeMount` will cause an error 500. This seems to be nuxt specific.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,60 @@
 <template>
-<div>Hello World</div>
+  <div>Hello World</div>
 </template>
 
 <script setup>
+import Surreal, {RecordId, Table} from "surrealdb.js";
 
+onBeforeMount(async () => {
+  const db = new Surreal();
+
+// Connect to the database
+  await db.connect("http://127.0.0.1:3001/rpc");
+
+// Select a specific namespace / database
+  await db.use({
+    namespace: "test",
+    database: "test"
+  });
+
+// Signin as a namespace, database, or root user
+  await db.signin({
+    username: "root",
+    password: "root",
+  });
+
+// Create a new person with a random id
+  let created = await db.create("person", {
+    title: "Founder & CEO",
+    name: {
+      first: "Tobie",
+      last: "Morgan Hitchcock",
+    },
+    marketing: true,
+  });
+
+  console.log(created)
+
+// Update a person record with a specific id
+  let updated = await db.merge(new RecordId('person', 'jaime'), {
+    marketing: true,
+  });
+
+  console.log(updated)
+
+// Select all people records
+  let people = await db.select("person");
+
+  console.log(people)
+
+// Perform a custom advanced query
+  let groups = await db.query(
+      "SELECT marketing, count() FROM $tb GROUP BY marketing",
+      {
+        tb: new Table("person"),
+      },
+  );
+
+  console.log(groups)
+})
 </script>
